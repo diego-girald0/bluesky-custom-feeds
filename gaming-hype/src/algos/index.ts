87e08debbd6e6cfd3c8f4116e636f92d5a937ec7
Diego Giraldo <67902366+diego-girald0@users.noreply.github.com>
2024-12-17 11:42:06 +0000

    feat(gaming-hype-feed): implement feed with keyword filtering logic

--- gaming-hype/src/algos/index.ts
+++ gaming-hype/src/algos/index.ts
@@ -4,11 +4,13 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as whatsAlf from './whats-alf'
+import * as gamingHype from './gaming-hype'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [whatsAlf.shortname]: whatsAlf.handler,
+  [gamingHype.shortname]: gamingHype.handler,
 }
 
 export default algos
